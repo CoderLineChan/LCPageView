6765ef07386998fd44d5065ea3183aee1a0fcc06
lianchen <lianchen551@163.com>
2018-06-13 00:01:49 +0800

    添加自定义headView

---
+++ Source/LCHeadView.h
@@ -0,0 +1,13 @@
+//
+//  LCHeadView.h
+//  Example
+//
+//  Created by 陈连辰 on 2018/6/11.
+//  Copyright © 2018年 linechan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCHeadView : UIView
+
+@end
---
+++ Source/LCHeadView.m
@@ -0,0 +1,21 @@
+//
+//  LCHeadView.m
+//  Example
+//
+//  Created by 陈连辰 on 2018/6/11.
+//  Copyright © 2018年 linechan. All rights reserved.
+//
+
+#import "LCHeadView.h"
+
+@implementation LCHeadView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
